a19334ca8a39ad6b6886524889f06c6b65c85a44
supanut <supanut.pgs@gmail.com>
2019-12-22 03:08:09 +0700

    Return http response code

--- src/Goosapi/Core/Response.php
+++ src/Goosapi/Core/Response.php
@@ -9,6 +9,7 @@ class Response implements IResponse
     {
         if (gettype($data) == "array" || gettype($data) == "object")
         {
+            http_response_code($code);
             header('Content-Type: application/json');
             echo json_encode($data);
         }
