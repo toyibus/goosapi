0f8e0ae19872602d9bf6bdbee27bf60a2375c470
supanut <supanut.pgs@gmail.com>
2020-01-03 01:16:43 +0700

    provide function to get environment data

--- src/Goosapi/Global.php
+++ src/Goosapi/Global.php
@@ -36,3 +36,24 @@ function getBearerToken() {
     }
     return null;
 }
+
+function env($key, $default = "")
+{
+    return findWithKey(".env", $key, $default);
+}
+
+function findWithKey($file, $key, $default = "")
+{
+    $contents = file_get_contents($file);
+    //$contents = explode(PHP_EOL, trim($contents));
+    $contents = preg_split('/\r\n|\r|\n/', $contents);
+    //dd($contents);
+    foreach ($contents as $c)
+    {
+        if (!$c) continue;
+        $line = explode('=', $c);
+   
+        if ($line[0]==$key) return $line[1];
+    }
+    return $default;
+}
