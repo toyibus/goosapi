d831ed594c0eb324eb4dbb18084f09fe1377204f
supanut <supanut.pgs@gmail.com>
2020-01-03 00:23:34 +0700

    Enable to setting CORs

--- src/Goosapi/goosapi.php
+++ src/Goosapi/goosapi.php
@@ -24,21 +24,22 @@ class goosapi implements IRouter
         
     }
 
-    public function allowCORs()
+    public function allowCORs($cache = 86400, ... $allows)
     {
         // ------------------ Allow CORs ----------------------
-
+        if (!$allows) $allows = "*";
+        $allows = implode(",", $allows);
         if (isset($_SERVER['HTTP_ORIGIN'])) 
         {
             header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
             header("Access-Control-Allow-Credentials: true");
-            header("Access-Control-Max-Age: 86400"); // cache for 1 day 
+            header("Access-Control-Max-Age: $cache"); // cache for 1 day 
         }
         
         if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') 
         {
             if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-                header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+                header("Access-Control-Allow-Methods: $allows");
         
             if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
                 header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
